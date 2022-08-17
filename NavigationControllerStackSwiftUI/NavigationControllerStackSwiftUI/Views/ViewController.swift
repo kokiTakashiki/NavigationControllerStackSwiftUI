6033628a9aa642a8841a2de7338c2ce520473934
kokiTakeda <bluewhitered123@gmail.com>
2022-08-18 01:06:16 +0900

    Start

---
+++ NavigationControllerStackSwiftUI/NavigationControllerStackSwiftUI/Views/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  NavigationControllerStackSwiftUI
+//
+//  Created by 武田孝騎 on 2022/08/17.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+    
+    private let config = Configuration()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "test navigation"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .lightGray
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = self.navigationController?.navigationBar.standardAppearance
+        
+        let viewController: UIHostingController = UIHostingController(rootView: SegmentPickerPageView(config: config))
+        config.hostingController = viewController
+        self.addChild(viewController)
+        self.view.addSubview(viewController.view)
+        viewController.didMove(toParent: self)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        viewController.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        viewController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        viewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
+        viewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
+        viewController.view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+}
+
